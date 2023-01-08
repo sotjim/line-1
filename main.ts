75dcdbd83caecd9aa3441e14b9e6f0bb40f8cf53
sotjim <122051655+sotjim@users.noreply.github.com>
2023-01-08 17:51:02 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+wuKong.setLightMode(wuKong.LightMode.BREATH)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P2) == 0) {
+        wuKong.setAllMotor(-40, 0)
+        if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 1) {
+            wuKong.setAllMotor(1, -39)
+        }
+        if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P2) == 1) {
+            wuKong.setAllMotor(-65, -65)
+        }
+    }
+})
