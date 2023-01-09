ca0577fa7d9f986f2431d6e71045c5c004fd3ed2
sotjim <122051655+sotjim@users.noreply.github.com>
2023-01-09 19:05:37 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,10 +10,10 @@ pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P2) == 0) {
-        wuKong.setAllMotor(-40, 0)
+        wuKong.setAllMotor(1, -18)
     } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 1) {
-        wuKong.setAllMotor(1, -40)
+        wuKong.setAllMotor(-12, 0)
     } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P2) == 0) {
-        wuKong.setAllMotor(-65, -65)
+        wuKong.setAllMotor(-23, -23)
     }
 })
